1ee5f08de15206c33cad3752fcdb35ad722eab66
Shridhar Mali <shridharmalimca@gmail.com>
2024-12-07 18:44:59 +0530

    generic button component using swiftUI

---
+++ ButtonDemo/ButtonDemo/ButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonDemoApp.swift
+//  ButtonDemo
+//
+//  Created by Macbook on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonDemo/ButtonDemoTests/ButtonDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonDemoTests.swift
+//  ButtonDemoTests
+//
+//  Created by Macbook on 07/12/24.
+//
+
+import Testing
+@testable import ButtonDemo
+
+struct ButtonDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
