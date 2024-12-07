b533f9832c07f45d6f5798e209f4614d172f36e5
Shridhar Mali <shridharmalimca@gmail.com>
2024-12-07 19:35:52 +0530

    button component with generic modifiers

--- ButtonDemo/ButtonDemo/ContentView.swift
+++ ButtonDemo/ButtonDemo/ContentView.swift
@@ -13,9 +13,51 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            SDLButton(title: "Click") {
+                debugPrint("Clicked")
+            } content: {
+                
+            }
+            .sdlButtonStyle()
         }
         .padding()
+    }
+}
+
+struct SDLButton<Content: View>:View {
+    
+    let title: String?
+    let action: () -> Void
+    let content: Content
+    
+    init(title: String?, action: @escaping () -> Void, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.action = action
+        self.content = content()
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            if let btnTitle = self.title {
+                Text(btnTitle)
+            }
+        }
+    }
+}
+
+struct SDLButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 5))
+    }
+}
+
+extension View {
+    func sdlButtonStyle() -> some View {
+        modifier(SDLButtonStyle())
     }
 }
 
